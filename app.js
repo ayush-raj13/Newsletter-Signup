35018f10f6223243c196cbdaf64fdee02b60543d
Ayush Raj <ayushraj.iiitbh@gmail.com>
2022-12-17 17:20:33 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const client = require("@mailchimp/mailchimp_marketing");
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/public/signup.html");
+});
+
+app.post("/", function(req, res){
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+    console.log(firstName + lastName + email);
+    client.setConfig({
+        apiKey: process.env.API_KEY,
+        server: "us21",
+      });
+      
+      const run = async () => {
+        const response = await client.lists.batchListMembers("6bf1f02a76", {
+          members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }],
+        });
+        console.log(response);
+        if (response.errors.length==0){
+            res.send("Successfully Subscribed!");
+        }else {
+            res.send("There was an error with signing up, " + response.errors[0].error_code + ", please try again!");
+        }
+      };
+      
+      run();
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server started on port 3000");
+});

4f0c8d0ace786c9d7e6423e46283c86edff5dd12
Ayush Raj <ayushraj.iiitbh@gmail.com>
2022-12-17 18:27:59 +0530

    small change 1

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const client = require("@mailchimp/mailchimp_marketing");
+const path = require("path");
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
